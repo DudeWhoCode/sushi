f4038ceb5d816dcb82b213263ab559a831205555
Naren <narencodes@gmail.com>
2019-06-24 10:11:01 +0530

    The programming langauge's name is boa 🐍

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/dudewhocode/interpreter/repl"
+)
+
+func main() {
+	fmt.Println("boa v0.0.1")
+	repl.Start(os.Stdin, os.Stdout)
 }
