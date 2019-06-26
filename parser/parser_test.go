990157eadb9a90dc30ec82366559f8bf4383ec4b
Naren <narencodes@gmail.com>
2019-06-26 09:18:25 +0530

    Test return statements

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -77,3 +77,31 @@ func checkParserErrors(t *testing.T, p *Parser) {
 	}
 	t.FailNow()
 }
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+	return 5;
+	return 10;
+	return 993322;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("expected 3 program.Statements. got=%d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("expected *ast.ReturnStatement, got %T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("expected returnStmnt.TokenLiteral to be 'return , got %q", returnStmt.TokenLiteral())
+		}
+	}
+}
