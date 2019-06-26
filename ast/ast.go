3c6b51b0a6ea042a285f42afde30eaa5dd82bd67
Naren <narencodes@gmail.com>
2019-06-26 08:05:12 +0530

    Abstract Syntax Tree nodes
    Statement and expression interface implements node interface. Program type satisfies all above interfaces

---
+++ ast/ast.go
@@ -0,0 +1,46 @@
+package ast
+
+import "github.com/dudewhocode/interpreter/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+type Identifier struct {
+	Token token.Token // token.IDENT
+	Value string
+}
+
+type LetStatement struct {
+	Token token.Token // token.LET
+	Name  *Identifier
+	Value Expression
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
