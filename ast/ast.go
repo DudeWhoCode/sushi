44426a1fcc3f9dc7dd2b1be7d241430b2ee976c3
Naren <narencodes@gmail.com>
2019-06-26 09:09:59 +0530

    Add return statement AST node

--- ast/ast.go
+++ ast/ast.go
@@ -31,6 +31,11 @@ type LetStatement struct {
 	Value Expression
 }
 
+type ReturnStatement struct {
+	Token       *token.Token // token.RETURN
+	ReturnValue Expression
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -44,3 +49,6 @@ func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
