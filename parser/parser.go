404714a45630f0f4c0de4dd930f4df0ec5474ae4
Naren <narencodes@gmail.com>
2019-06-26 08:49:10 +0530

    While peeking, get errors and store it
    For now parser doesn't stop when error is occurred but stores the errors instead for reference

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/dudewhocode/interpreter/ast"
 	"github.com/dudewhocode/interpreter/lexer"
 	"github.com/dudewhocode/interpreter/token"
@@ -11,10 +13,13 @@ type Parser struct {
 
 	curToken  *token.Token
 	peekToken *token.Token
+	errors    []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l,
+		errors: []string{},
+	}
 
 	// Read two tokesn, so curToken and peekToken are set
 	p.nextToken()
@@ -83,7 +88,16 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 	if p.peekTokenIs(t) {
 		p.nextToken()
 		return true
-	} else {
-		return false
 	}
+	p.peekError(t)
+	return false
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
