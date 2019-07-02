a35c1ad69db731746cdf038c0a1faef0773c8869
Naren <narencodes@gmail.com>
2019-07-02 16:45:23 +0530

    Add AST nodes for function

--- ast/ast.go
+++ ast/ast.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"bytes"
+	"strings"
 
 	"github.com/dudewhocode/interpreter/token"
 )
@@ -79,6 +80,12 @@ type IfExpression struct {
 	Condition   Expression
 	Consequence *BlockStatement
 	Alternative *BlockStatement
+}
+
+type FunctionLiteral struct {
+	Token      *token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
 }
 
 func (p *Program) TokenLiteral() string {
@@ -213,5 +220,25 @@ func (bs *BlockStatement) String() string {
 	for _, s := range bs.Statements {
 		out.WriteString(s.String())
 	}
+	return out.String()
+}
+
+func (fl *FunctionLiteral) statementNode()       {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ","))
+	out.WriteString(")")
+	out.WriteString(fl.Body.String())
+
 	return out.String()
 }
