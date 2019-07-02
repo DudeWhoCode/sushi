6c8821c94fee3ca37ffd4bcdb6470a641858e32b
Naren <narencodes@gmail.com>
2019-07-02 11:48:56 +0530

    Helper function to test boolean literal

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -383,3 +383,18 @@ func testInfixExpression(t *testing.T, exp ast.Expression, left interface{}, ope
 	}
 	return true
 }
+
+func testBooleanLiteral(t *testing.T, exp ast.Expression, value bool) bool {
+	bo, ok := exp.(*ast.Boolean)
+	if !ok {
+		t.Errorf("exp not *ast.Boolean. got=%T", exp)
+		return false
+	}
+	if bo.Value != value {
+		t.Errorf("bo.Value not %t. got=%t", value, bo.Value)
+	}
+	if bo.TokenLiteral() != fmt.Sprintf("%t", value) {
+		t.Errorf("bo.TokenLiteral not %t. got=%s", value, bo.TokenLiteral())
+	}
+	return true
+}
