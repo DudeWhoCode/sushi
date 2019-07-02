9b257461d88a102111755657449d576b0bf3109d
Naren <narencodes@gmail.com>
2019-07-02 11:48:26 +0530

    Add AST nodes for bool values

--- ast/ast.go
+++ ast/ast.go
@@ -64,6 +64,11 @@ type InfixExpression struct {
 	Right    Expression
 }
 
+type Boolean struct {
+	Token *token.Token
+	Value bool
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -163,3 +168,7 @@ func (ie *InfixExpression) String() string {
 
 	return out.String()
 }
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
