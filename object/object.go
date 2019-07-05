6a7f0c5f387ca3a4576a5e9a61bb960fb628fee7
Naren <narencodes@gmail.com>
2019-07-05 13:30:36 +0530

    Define boolean objects

--- object/object.go
+++ object/object.go
@@ -6,6 +6,7 @@ type ObjectType string
 
 const (
 	INTEGEROBJ = "INTEGER"
+	BOOLEANOBJ = "BOOLEAN"
 )
 
 type Object interface {
@@ -17,5 +18,12 @@ type Integer struct {
 	Value int64
 }
 
+type Boolean struct {
+	Value bool
+}
+
 func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
 func (i *Integer) Type() string    { return INTEGEROBJ }
+
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) Type() string    { return BOOLEANOBJ }
