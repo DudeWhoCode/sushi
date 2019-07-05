fc63672e7b3268d145e3702e429643f14362575c
Naren <narencodes@gmail.com>
2019-07-05 15:42:00 +0530

    Evaluate the ast node recursively by deciding node type

---
+++ evaluator/eval.go
@@ -0,0 +1,30 @@
+package evaluator
+
+import (
+	"github.com/dudewhocode/interpreter/ast"
+
+	"github.com/dudewhocode/interpreter/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
+}
