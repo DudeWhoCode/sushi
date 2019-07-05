01db100afd8bdd131a92c211b2d32382e476d2c1
Naren <narencodes@gmail.com>
2019-07-05 15:42:25 +0530

    Pass the parsed ast nodes to evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/dudewhocode/interpreter/evaluator"
 	"github.com/dudewhocode/interpreter/lexer"
 	"github.com/dudewhocode/interpreter/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
