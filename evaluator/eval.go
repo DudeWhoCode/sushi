5ffc481c24c749f8b65f3a585afdcfbbc0380dfb
Naren <narencodes@gmail.com>
2019-07-07 14:01:12 +0530

    Evaluate bang operator
    First evaluate the expression on right, once it returns a boolean, decide what to return

--- evaluator/eval.go
+++ evaluator/eval.go
@@ -25,6 +25,9 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBoolObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 	return nil
 }
@@ -42,4 +45,26 @@ func nativeBoolToBoolObject(input bool) *object.Boolean {
 		return TRUE
 	}
 	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return FALSE
+	default:
+		return FALSE
+	}
 }
