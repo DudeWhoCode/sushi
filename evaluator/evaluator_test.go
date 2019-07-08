b4d3deedf5bfb958fb0128ced431fde24e3ba718
Naren <narencodes@gmail.com>
2019-07-08 20:58:25 +0530

    Test let statements by tracking identifiers

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -189,6 +189,22 @@ func TestErrorHandling(t *testing.T) {
 	}
 }
 
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
