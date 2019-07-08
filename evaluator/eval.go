5aa7b674a83285d0f3b06d555a8f97e754e773eb
Naren <narencodes@gmail.com>
2019-07-08 12:49:15 +0530

    Evaluate integer infix expressions that returns boolean

--- evaluator/eval.go
+++ evaluator/eval.go
@@ -106,6 +106,14 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return &object.Integer{Value: leftVal * rightVal}
 	case "/":
 		return &object.Integer{Value: leftVal / rightVal}
+	case "<":
+		return nativeBoolToBoolObject(leftVal < rightVal)
+	case ">":
+		return nativeBoolToBoolObject(leftVal > rightVal)
+	case "==":
+		return nativeBoolToBoolObject(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBoolObject(leftVal != rightVal)
 	default:
 		return NULL
 	}
