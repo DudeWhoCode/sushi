2ccb4455a1beef6198fc35de3270af86e0281dc1
Naren <narencodes@gmail.com>
2019-07-08 13:33:07 +0530

    Evaluate return statements by checking if result is a return object

--- evaluator/eval.go
+++ evaluator/eval.go
@@ -21,6 +21,9 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.BlockStatement:
 		return evalStatements(node.Statements)
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue)
+		return &object.ReturnValue{Value: val}
 
 	// expressions
 	case *ast.IntegerLiteral:
@@ -44,6 +47,10 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	var result object.Object
 	for _, statement := range stmts {
 		result = Eval(statement)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
 	}
 	return result
 }
