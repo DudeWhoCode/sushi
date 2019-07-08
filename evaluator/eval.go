014f841e69af2b4a042d54bbd3125bf4c4c5c3f9
Naren <narencodes@gmail.com>
2019-07-08 13:15:38 +0530

    Evaluate if else expressions
    Decide which block to execute by truthiness of the condition

--- evaluator/eval.go
+++ evaluator/eval.go
@@ -19,6 +19,8 @@ func Eval(node ast.Node) object.Object {
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
 
 	// expressions
 	case *ast.IntegerLiteral:
@@ -32,6 +34,8 @@ func Eval(node ast.Node) object.Object {
 		left := Eval(node.Left)
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	}
 	return nil
 }
@@ -122,5 +126,29 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return nativeBoolToBoolObject(leftVal != rightVal)
 	default:
 		return NULL
+	}
+}
+
+func evalIfExpressoin(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
 	}
 }
