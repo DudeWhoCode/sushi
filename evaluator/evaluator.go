fac53f9dc241243a167a69271bd79cd9641e25eb
Naren <narencodes@gmail.com>
2019-07-09 10:57:40 +0530

    Eval function literals

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -61,6 +61,10 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return evalIfExpression(node, env)
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
 	}
 	return nil
 }
