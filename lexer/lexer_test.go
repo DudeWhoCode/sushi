53d6d13624988d6e8b5a300c71de2bfe1a6bc010
Naren <narencodes@gmail.com>
2019-07-09 21:58:38 +0530

    Test string tokens

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -26,6 +26,8 @@ func TestNextToken(t *testing.T) {
 
 	10 == 10;
 	10 != 9;
+	"foobar"
+	"foo bar"
 	`
 
 	tests := []struct {
@@ -105,6 +107,8 @@ func TestNextToken(t *testing.T) {
 		{token.NOTEQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
 		{token.EOF, ""},
 	}
 
