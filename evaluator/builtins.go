8458052e1f4fc1f735bb64a418b62460ad84440c
Naren <narencodes@gmail.com>
2019-07-10 14:15:24 +0530

    Add new environment for builtin functions

---
+++ evaluator/builtins.go
@@ -0,0 +1,13 @@
+package evaluator
+
+import (
+	"github.com/dudewhocode/boa/object"
+)
+
+var builtins = map[string]*object.Builtin{
+	"len": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			return NULL
+		},
+	},
+}
