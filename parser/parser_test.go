acef582701fe80ac5909a74aafeb6f77b98d7eeb
Naren <narencodes@gmail.com>
2019-07-10 22:14:40 +0530

    Test index operator precedence

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -365,6 +365,14 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 			"add(a + b + c * d / f + g)",
 			"add((((a + b) + ((c * d) / f)) + g))",
 		},
+		{
+			"a * [1, 2, 3, 4][b * c] * d",
+			"((a * ([1, 2, 3, 4][b * c])) * d)",
+		},
+		{
+			"add(a * b[2], b[1], 2 * [1, 2][1])",
+			"add((a * (b[2])), (b[1]), (2 * ([1, 2][1])))",
+		},
 	}
 
 	for _, tt := range tests {
