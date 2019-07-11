127b5f147ed78b624d5ca87ddcc82d09116f8bc3
Naren <narencodes@gmail.com>
2019-07-11 13:32:03 +0530

    Add first() Array builtin
    first() function returns the first element in an Array

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -20,4 +20,20 @@ var builtins = map[string]*object.Builtin{
 			}
 		},
 	},
+	"first": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAYOBJ {
+				return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+			return NULL
+		},
+	},
 }
