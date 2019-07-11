05e26437cd9d98c346158b4048c082f00ddcf5c6
Naren <narencodes@gmail.com>
2019-07-11 13:27:44 +0530

    Extend len() builtin to support arrays

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -13,6 +13,8 @@ var builtins = map[string]*object.Builtin{
 			switch arg := args[0].(type) {
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			default:
 				return newError("argument to `len` not supported, got %s", args[0].Type())
 			}
