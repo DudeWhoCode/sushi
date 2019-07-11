46a76794e9e95f5c808303af08ce541d4ab3f6b5
Naren <narencodes@gmail.com>
2019-07-11 13:37:43 +0530

    Add rest() Array builtin
    rest() function returns the rest of the elements as a new Array expect first element

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -53,4 +53,23 @@ var builtins = map[string]*object.Builtin{
 			return NULL
 		},
 	},
+	"rest": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAYOBJ {
+				return newError("argument to `rest` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				newElements := make([]object.Object, length-1, length-1)
+				copy(newElements, arr.Elements[1:length])
+				return &object.Array{Elements: newElements}
+			}
+			return NULL
+		},
+	},
 }
