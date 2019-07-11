8bdc46febdc4c93e4811ba662773381e4de3026a
Naren <narencodes@gmail.com>
2019-07-11 13:33:18 +0530

    Add last() Array builtin
    last() function returns the last element in an Array

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -36,4 +36,21 @@ var builtins = map[string]*object.Builtin{
 			return NULL
 		},
 	},
+	"last": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAYOBJ {
+				return newError("argument to `last` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				return arr.Elements[length-1]
+			}
+			return NULL
+		},
+	},
 }
