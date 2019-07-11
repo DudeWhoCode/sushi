1a0c6aa8aa4799d7fe8995e255f2ab40e15d27ce
Naren <narencodes@gmail.com>
2019-07-11 13:48:30 +0530

    Add push() Array builtin
    push() function appends the element to an array by creating a new array

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -72,4 +72,21 @@ var builtins = map[string]*object.Builtin{
 			return NULL
 		},
 	},
+	"push": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2", len(args))
+			}
+			if args[0].Type() != object.ARRAYOBJ {
+				return newError("argument to `push` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			newElements := make([]object.Object, length+1, length+1)
+			copy(newElements, arr.Elements)
+			newElements[length] = args[1] // append the item
+			return &object.Array{Elements: newElements}
+		},
+	},
 }
