0c5a8a8218d87b9f3a679689ea62db95045fe583
Naren <narencodes@gmail.com>
2019-07-13 17:42:29 +0530

    Evalute hash retrieval

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -267,6 +267,8 @@ func evalIndexExpression(left, index object.Object) object.Object {
 	switch {
 	case left.Type() == object.ARRAYOBJ && index.Type() == object.INTEGEROBJ:
 		return evalArrayIndexExpression(left, index)
+	case left.Type() == object.HASHOBJ:
+		return evalHashIndexExpression(left, index)
 	default:
 		return newError("index operator not supported: %s", left.Type())
 	}
@@ -308,6 +310,21 @@ func evalHashLiteral(node *ast.HashLiteral, env *object.Environment) object.Obje
 	}
 
 	return &object.Hash{Pairs: pairs}
+}
+
+func evalHashIndexExpression(hash, index object.Object) object.Object {
+	hashObject := hash.(*object.Hash)
+
+	key, ok := index.(object.Hashable)
+	if !ok {
+		return newError("unhashable key: %s", index.Type())
+	}
+
+	pair, ok := hashObject.Pairs[key.HashKey()]
+	if !ok {
+		return NULL
+	}
+	return pair.Value
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
