4f891629596e18ee17c63ee166851281b3d31aa0
Naren <narencodes@gmail.com>
2019-07-16 17:39:57 +0530

    Test tokenizing floating point numbers

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -30,6 +30,8 @@ func TestNextToken(t *testing.T) {
 	"foo bar"
 	[1, 2];
 	{"foo": "bar"}
+	10.25
+	let f = 20.543
 	`
 
 	tests := []struct {
@@ -122,6 +124,11 @@ func TestNextToken(t *testing.T) {
 		{token.COLON, ":"},
 		{token.STRING, "bar"},
 		{token.RBRACE, "}"},
+		{token.FLOAT, "10.25"},
+		{token.LET, "let"},
+		{token.IDENT, "f"},
+		{token.ASSIGN, "="},
+		{token.FLOAT, "20.543"},
 		{token.EOF, ""},
 	}
 
